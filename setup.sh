c691b7fa4038fba7c6e948ecfc7ebd2b654e6800
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-09-25 11:48:00 +0100

    Add context comments to setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# setup code server
 cat > install-code.sh << 'EOF'
 curl -fsSL https://code-server.dev/install.sh | sh
 sudo systemctl enable --now code-server@$USER
@@ -16,11 +17,15 @@ sudo gpasswd -a student docker
 sudo apt-get update
 # install common dependencies
 sudo apt-get install -y git less groff unzip python3 python3-pip python3-venv
+# install ansible
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 ansible-galaxy collection install amazon.aws
+# install aws and antigravity CLIs
 curl -fsSL https://awscli.amazonaws.com/v2/install.sh | bash
 curl -fsSL https://antigravity.google/cli/install.sh | bash
+# add typical aws binary location to path
 echo "export PATH=$PATH:$HOME/.local/bin" | tee -a $HOME/.bashrc
+# install postgres CLI
 sudo apt-get install -y postgresql-client
