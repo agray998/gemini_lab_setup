0c783e9a18667511d33e3a4df804751715b057ba
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-09-25 08:35:24 +0100

    Replace pgAdmin installation with PostgreSQL client

--- setup.sh
+++ setup.sh
@@ -18,6 +18,4 @@ ansible-galaxy collection install amazon.aws
 curl -fsSL https://awscli.amazonaws.com/v2/install.sh | bash
 curl -fsSL https://antigravity.google/cli/install.sh | bash
 echo "export PATH=$PATH:$HOME/.local/bin" | tee -a $HOME/.bashrc
-curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /etc/apt/keyrings/packages-pgadmin-org.gpg
-sudo sh -c 'echo "deb [signed-by=/etc/apt/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt-get update'
-sudo apt-get install pgadmin4
+sudo apt-get install -y postgresql-client-18
